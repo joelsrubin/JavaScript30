b3923fd3d9c7f48739f6979d2c43c192fdeba79d
joelsrubin <joelsrubin@gmail.com>
2021-03-13 22:06:10 -0600

    ok

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,17 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      let url = URL.createObjectURL(localMediaStream);
+      video.src = url;
+      video.play();
+    })
+    .catch(err => console.error(err));
+}
+
+getVideo();
